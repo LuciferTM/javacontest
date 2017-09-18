2b63d39a0a15e9f12dd0e1fb89498167b74b392a
LuciferTM <roy_qian@gmail.com>
2017-09-18 16:48:19 +0800

    Sechedule Thread Pool

---
+++ src/main/java/com/lucifer/javacontest/ch3/pool/ScheduledExecutorServiceDemo.java
@@ -0,0 +1,28 @@
+
+package com.lucifer.javacontest.ch3.pool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorServiceDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService ses=Executors.newScheduledThreadPool(10);
+        ses.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println(System.currentTimeMillis()/1000);
+//                    if(System.currentTimeMillis()%2==0){
+//                    	System.out.println("exception");
+//                    	throw new RuntimeException();
+//                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }                
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+        
+    }
+}
