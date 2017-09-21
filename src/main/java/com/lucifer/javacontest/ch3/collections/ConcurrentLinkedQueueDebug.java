3324e5c0e5ea3fdaa0a314be02038faaccedbf7a
royqian1988@gmail.com <roy_qian1988@gmail.com>
2017-09-21 14:58:04 +0800

    测试绑定email

---
+++ src/main/java/com/lucifer/javacontest/ch3/collections/ConcurrentLinkedQueueDebug.java
@@ -0,0 +1,13 @@
+
+package com.lucifer.javacontest.ch3.collections;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ConcurrentLinkedQueueDebug {
+    public static void main(String[] args) {
+        ConcurrentLinkedQueue<String> q=new ConcurrentLinkedQueue<String>();
+        q.add("1");
+        q.poll();
+        q.add("3");
+    }
+}
